75f7014109bf48d7cd2b3dc8617e17ffaab7dc1f
ThomasSingh222 <tryingtoelevate3171992@gmail.com>
2026-09-06 09:07:36 +0530

    local changes

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,11 @@ use std::net::SocketAddr;
 use warp::Filter;
 
 use simulation::core::simulation::Simulation;
-
+ 
 
 #[tokio::main]
 async fn main() {
-    let routes = warp::any().map(|| "BC Simulation Chal peya OA!!");
+    let routes = warp::any().map(|| "BC Simulation Chal peya OA !!");
 
     let mut simulation = Simulation::new(4);
     tokio::task::spawn_blocking(move || simulation.run());
@@ -15,4 +15,4 @@ async fn main() {
     let addr: SocketAddr = "127.0.0.1:3030".parse().unwrap();
     warp::serve(routes).run(addr).await;  
     
-}
+} 
